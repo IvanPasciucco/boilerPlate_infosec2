aac01782eecbd4ad9e3dbad0d5214055882947fe
IvanPasciucco <54645974+IvanPasciucco@users.noreply.github.com>
2025-12-14 02:54:35 -0300

    first commit4

--- myApp.js
+++ myApp.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
+
+// 1. Importar Helmet
 const helmet = require('helmet');
 
+// 2. Usar hidePoweredBy (EL NUEVO REQUISITO)
+app.use(helmet.hidePoweredBy());
+
 module.exports = app;
-// const api = require('./server.js');
+
+// Configuración de estáticos
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-// app.use('/_api', api);
+
+// Rutas
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+// Listener
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
